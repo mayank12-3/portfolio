54db911b1a95725ee0869f300ec2c7490cd5c80b
deepakc <deepak.c@servify.in>
2020-03-31 17:10:18 +0530

    added page loader

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import {
   Layout,
   Header,
@@ -9,15 +9,28 @@ import './App.scss'
 import { scrollFunc } from './components/animationFunc'
 
 export const App = () => {
+  const [showLoader, loaderStatus] = useState(true)
+
   useEffect(() => {
     window.addEventListener('scroll', scrollFunc)
+    setTimeout(() => {
+      loaderStatus(false)
+    }, 2000)
   })
   return (
     <div className='App'>
-      <Layout>
-        <Header />
-        <Content />
-      </Layout>
+      {showLoader
+        ? <div className='page-loader'>
+          <div className='loader-content'>
+            D
+            <div class='spinner-border text-light' role='status' />
+          </div>
+        </div>
+        : <Layout>
+          <Header />
+          <Content />
+        </Layout>
+      }
     </div>
   )
 }

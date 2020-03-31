9d608ea817c3372ee92bff91f15a26ea856ad4dc
deepakc <deepak.c@servify.in>
2020-03-31 17:10:36 +0530

    styling for mobile nav bar

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -2,25 +2,47 @@ import React from 'react'
 import logo from '../../assets/logo.jpg'
 import './index.scss'
 
+function toggleMobileNavBar(value) {
+  document.getElementById("mobile-view-navbar").style.height = value
+}
+
 function Header() {
   return (
-    <nav className='navbar fixed-top dark-mode'>
-      <a className='navbar-brand dark-mode' href='#'><img src={logo} alt='logo' /></a>
-      <ul className='nav justify-content-end'>
-        <li className='nav-item'>
-          <a className='nav-link dark-mode' href='#' data-text='Home'>Home</a>
-        </li>
-        <li className='nav-item'>
-          <a className='nav-link dark-mode' href='#' data-text='Work'>Work</a>
-        </li>
-        <li className='nav-item nav-last-item'>
-          <a className='nav-link dark-mode' href='#' data-text='About Me'>About Me</a>
-        </li>
-        <li className='nav-item nav-drop-button nav-last-item'>
-          {[1, 2, 3].map((val) => <div key={val} className='toggle-dark-mode' />)}
-        </li>
-      </ul>
-    </nav>
+    <React.Fragment>
+      <div id='mobile-view-navbar' className='toggle-dark-mode'>
+        <div className='mobile-view-navbar__close-icon toggle-dark-mode' onClick={() => toggleMobileNavBar('0%')}>
+          &times;
+        </div>
+        <ul>
+          <li>
+            <a className='nav-link toggle-dark-mode' href='#' data-text='Home'>Home</a>
+          </li>
+          <li>
+            <a className='nav-link toggle-dark-mode' href='#' data-text='Work'>Work</a>
+          </li>
+          <li>
+            <a className='nav-link toggle-dark-mode' href='#' data-text='About Me'>About Me</a>
+          </li>
+        </ul>
+      </div>
+      <nav className='navbar fixed-top dark-mode'>
+        <a className='navbar-brand dark-mode' href='#'><img src={logo} alt='logo' /></a>
+        <ul className='nav justify-content-end overlay-content'>
+          <li className='nav-item'>
+            <a className='nav-link dark-mode' href='#' data-text='Home'>Home</a>
+          </li>
+          <li className='nav-item'>
+            <a className='nav-link dark-mode' href='#' data-text='Work'>Work</a>
+          </li>
+          <li className='nav-item nav-last-item'>
+            <a className='nav-link dark-mode' href='#' data-text='About Me'>About Me</a>
+          </li>
+          <li className='nav-item nav-drop-button nav-last-item' onClick={() => toggleMobileNavBar('100%')}>
+            {[1, 2, 3].map((val) => <div key={val} className='toggle-dark-mode' />)}
+          </li>
+        </ul>
+      </nav>
+    </React.Fragment>
   )
 }
 
